2f734b38cfc416c892a2c6eb061d8f9b223359ee
SeungaLeeah <92766716+SeungaLeeah@users.noreply.github.com>
2022-07-11 10:35:59 +0900

    backend

---
+++ 07_Node.js/14_RestfulAPI/app.js
@@ -0,0 +1,147 @@
+/*----------------------------------------------------------
+ | 1) 모듈참조
+ -----------------------------------------------------------*/
+/** 직접 구현한 모듈  */
+import logger from './helper/LogHelper.js';
+import { myip, urlFormat } from './helper/UtilHelper.js';
+import WebHelper from './helper/WebHelper.js'
+import DBPool from './helper/DBPool.js';
+/** 내장모듈 */
+import url from 'url';
+import path from 'path';
+/** 설치가 필요한 모듈 */
+import dotenv from "dotenv";
+import express from 'express'; // Express 본체
+import useragent from 'express-useragent'; // 클라이언트의 정보를 조회할 수 있는 기능
+import serveStatic from 'serve-static'; // 특정 폴더의 파일을 URL로 노출시킴
+import serveFavicon from 'serve-favicon'; // favicon 처리
+import bodyParser from 'body-parser'; // POST 파라미터 처리
+import methodOverride from 'method-override'; // PUT 파라미터 처리
+import cookieParser from 'cookie-parser'; // Cookie 처리
+import expressSession from 'express-session'; // Session 처리
+import cors from 'cors';    //cors 패키지 참조
+/** 예외처리 관련 클래스 */
+import PageNotFoundException from './exceptions/PageNotFoundException.js'
+import DepartmentController from './controllers/DepartmentController.js';
+/** URL을 라우팅하는 모듈 참조*/
+
+
+/*---------------------------------`-------------------------
+ | 2) Express 객체 생성
+ -----------------------------------------------------------*/
+// 여기서 생성한 app 객체의 use() 함수를 사용해서
+// 각종 외부 기능, 설정 내용, URL을 계속해서 확장하는 형태로 구현이 진행된다.
+const app = express();
+
+// 프로젝트 폴더 위치
+const __dirname = path.resolve();
+
+// 설정 파일 내용 가져오기
+dotenv.config({ path: path.join(__dirname, "../config.env") });
+
+/*----------------------------------------------------------
+ | 3) 클라이언트의 접속시 초기화
+ -----------------------------------------------------------*/
+/** app 객체에 UserAgent 모듈을 탑재 */
+// --> Express객체(app)에 추가되는 확장 기능들을 Express에서는 미들웨어라고 부른다.
+// --> UserAgent 모듈은 초기화 콜백함수에 전달되는 req, res객체를 확장하기 때문에 다른 모듈들보다 먼저 설정되어야 한다.
+app.use(useragent.express());
+
+// 클라이언트의 접속을 감지
+app.use((req, res, next) => {
+    logger.debug('클라이언트가 접속했습니다.');
+
+    // 클라이언트가 접속한 시간
+    const beginTime = Date.now();
+
+    // 클라이언트의 IP주소(출처: 스택오버플로우)
+    const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress || req.socket.remoteAddress || req.connection.socket.remoteAddress;
+
+
+    // 클라이언트의 디바이스 정보 기록 (UserAgent 사용)
+    logger.debug(`[client] ${ip} / ${req.useragent.os} / ${req.useragent.browser} (${req.useragent.version}) / ${req.useragent.platform}`);
+
+    // 클라이언트가 요청한 페이지 URL
+    // 콜백함수에 전달되는 req 파라미터는 클라이언트가 요청한 URL의 각 부분을 변수로 담고 있다.
+    const current_url = urlFormat({
+        protocol: req.protocol, // ex) http://
+        host: req.get('host'), // ex) 172.16.141.1
+        port: req.port, // ex) 3000
+        pathname: req.originalUrl, // ex) /page1.html
+    });
+
+    logger.debug(`[${req.method}] ${decodeURIComponent(current_url)}`);
+
+    // 클라이언트의 접속이 종료된 경우의 이벤트 --> 모든 응답의 전송이 완료된 경우
+    res.on('finish', () => {
+        // 접속 종료시간
+        const endTime = Date.now();
+
+        // 이번 접속에서 클라이언트가 머문 시간 = 백엔드가 실행하는게 걸린 시간
+        const time = endTime - beginTime;
+        logger.debug(`클라이언트의 접속이 종료되었습니다. ::: [runtime] ${time}ms`);
+        logger.debug('--------------------------------------------------');
+    });
+
+    // 이 콜백함수를 종료하고 요청 URL에 연결된 기능으로 제어를 넘김
+    next();
+});
+    // Ctrl+C를 눌렀을때의 이벤트
+    process.on('SIGINT', () => {
+    process.exit();
+    });
+    // 프로그램이 종료될 때의 이벤트
+    process.on('exit', () => {
+    DBPool.close();
+    logger.info('-------- Server is close -------');
+    });
+
+/*----------------------------------------------------------
+ | 4) Express 객체의 추가 설정
+ -----------------------------------------------------------*/
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text()); // TEXT형식의 파라미터 수신 가능.
+app.use(bodyParser.json()); // JSON형식의 파라미터 수신 가능.
+app.use(methodOverride('X-HTTP-Method')); // Microsoft
+app.use(methodOverride('X-HTTP-Method-Override')); // Google/GData
+app.use(methodOverride('X-Method-Override')); // IBM
+app.use(methodOverride('_method')); // HTML form
+app.use(cookieParser(process.env.COOKIE_ENCRYPT_KEY));
+app.use(expressSession({secret: process.env.SESSION_ENCRYPT_KEY, resave: false,saveUninitialized: false}));
+app.use('/', serveStatic(process.env.PUBLIC_PATH));
+app.use(process.env.UPLOAD_URL, serveStatic(process.env.UPLOAD_DIR));
+app.use(process.env.THUMB_URL, serveStatic(process.env.THUMB_DIR));
+app.use(serveFavicon(process.env.FAVICON_PATH));
+app.use(WebHelper());
+app.use(cors());
+
+
+/*----------------------------------------------------------
+ | 5) 각 URL별 백엔드 기능 정의
+ -----------------------------------------------------------*/
+app.use(DepartmentController());
+
+app.use((err, req, res, next) => res.sendError(err));
+app.use("*", (req, res, next)=> res.sendError(new PageNotFoundException()));
+
+/** 컨트롤러에서 에러 발생 시 `next(에러객체)`를 호출했을 때 동잘할 처리 */
+app.use((err, req, res, next) => res.sendError(err));
+
+/** 앞에서 정의하지 않은 기타 URL에 대한 일괄처리 (무조건 맨 마지막에 정의해야 함) */
+app.use("*", (req, res, next)=>res.sendError(new PageNotFoundException()));
+/*----------------------------------------------------------
+ | 6) 설정한 내용을 기반으로 서버 구동 시작
+ -----------------------------------------------------------*/
+const ip = myip();
+
+app.listen(process.env.PORT, () => {
+    logger.debug('--------------------------------------------------');
+    logger.debug('|              Start Express Server              |');
+    logger.debug('--------------------------------------------------');
+
+    ip.forEach((v, i) => {
+        logger.debug(`server address => http://${v}:${process.env.PORT}`);
+    });
+
+    logger.debug('--------------------------------------------------');
+});
